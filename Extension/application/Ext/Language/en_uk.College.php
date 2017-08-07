a16a30ad00f8a5ddcc3df373720b0618d8101c09
ateeqrt <ateeq.rehman@rolustech.net>
2017-08-07 17:34:41 +0500

    Language

---
+++ Extension/application/Ext/Language/en_uk.College.php
@@ -0,0 +1,14 @@
+<?php
+$app_list_strings['moduleList']['clg_Programs'] 	= 'Programs';
+$app_list_strings['moduleList']['clg_Disciplines'] 	= 'Disciplines';
+$app_list_strings['moduleList']['clg_Students'] 	= 'Students';
+$app_list_strings['moduleList']['clg_Faculty'] 		= 'Faculty';
+$app_list_strings['moduleList']['clg_Sections'] 	= 'Sections';
+$app_list_strings['moduleList']['clg_Library'] 		= 'Library';
+
+$app_list_strings['moduleListSingular']['clg_Programs'] 	= 'Program';
+$app_list_strings['moduleListSingular']['clg_Disciplines'] 	= 'Discipline';
+$app_list_strings['moduleListSingular']['clg_Students'] 	= 'Student';
+$app_list_strings['moduleListSingular']['clg_Faculty'] 		= 'Faculty';
+$app_list_strings['moduleListSingular']['clg_Sections'] 	= 'Section';
+$app_list_strings['moduleListSingular']['clg_Library'] 		= 'Library';
