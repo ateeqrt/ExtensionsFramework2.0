2addfa24900d865c1254b0889c5f9d9b5202adc2
ateeqrt <ateeq.rehman@rolustech.net>
2017-08-07 12:00:28 +0500

    Extensions Added

---
+++ Extension/application/Ext/Extensions/anotherNotSoAwesomeExtension.php
@@ -0,0 +1,7 @@
+<?php
+$extensions['notSoAwesomeExtension'] = array(
+	'section' 	=> 'example',
+	'extdir' 	=> 'Example',
+	'file' 		=> 'example.ext.php',
+	'module' 	=> 'application/'
+	);
---
+++ application/Ext/Extensions/extensions.ext.php
@@ -0,0 +1,13 @@
+<?php
+// WARNING: The contents of this file are auto-generated.
+?>
+<?php
+// Merged from custom/Extension/application/Ext/Extensions/anotherNotSoAwesomeExtension.php
+
+$extensions['notSoAwesomeExtension'] = array(
+	'section' 	=> 'example',
+	'extdir' 	=> 'Example',
+	'file' 		=> 'example.ext.php',
+	'module' 	=> 'application/'
+	);
+?>
