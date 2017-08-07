2c0f74084825d939a2c5ea0827aeef80fa099419
ateeqrt <ateeq.rehman@rolustech.net>
2017-08-07 14:59:20 +0500

    Wireless Module Registry Added

---
+++ Extension/application/Ext/WirelessModuleRegistry/notSoAwesomeModule.php
@@ -0,0 +1,4 @@
+<?php
+$wireless_module_registry['custom_module'] = array(
+	'disable_create' => false,
+	);
---
+++ application/Ext/WirelessModuleRegistry/wireless_module_registry.ext.php
@@ -0,0 +1,10 @@
+<?php
+// WARNING: The contents of this file are auto-generated.
+?>
+<?php
+// Merged from custom/Extension/application/Ext/WirelessModuleRegistry/notSoAwesomeModule.php
+
+$wireless_module_registry['custom_module'] = array(
+	'disable_create' => false,
+	);
+?>
