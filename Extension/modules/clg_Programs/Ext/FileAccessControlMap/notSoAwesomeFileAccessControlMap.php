636c140e3e0e0ff34419586003c754c66b807387
ateeqrt <ateeq.rehman@rolustech.net>
2017-08-07 12:21:56 +0500

    File Access Control Added

---
+++ Extension/modules/clg_Programs/Ext/FileAccessControlMap/notSoAwesomeFileAccessControlMap.php
@@ -0,0 +1,4 @@
+<?php
+$file_access_control_map['modules']['clg_programs']['actions'] = array(
+	'detailView',
+	);
---
+++ modules/clg_Programs/Ext/FileAccessControlMap/file_access_control_map.ext.php
@@ -0,0 +1,10 @@
+<?php
+// WARNING: The contents of this file are auto-generated.
+?>
+<?php
+// Merged from custom/Extension/modules/clg_Programs/Ext/FileAccessControlMap/notSoAwesomeFileAccessControlMap.php
+
+$file_access_control_map['modules']['clg_programs']['actions'] = array(
+	'detailView',
+	);
+?>
