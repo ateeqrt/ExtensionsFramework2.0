234717a4ba2ad1e6fa071578ee6e1c9896431c80
ateeqrt <ateeq.rehman@rolustech.net>
2017-08-08 10:30:19 +0500

    Programs-Disciplines Many-to-Many

---
+++ Extension/modules/clg_Programs/Ext/clients/mobile/layouts/subpanels/clg_programs_clg_disciplines_1_clg_Programs.php
@@ -0,0 +1,10 @@
+<?php
+// created: 2017-08-08 10:29:27
+$viewdefs['clg_Programs']['mobile']['layout']['subpanels']['components'][] = array (
+  'layout' => 'subpanel',
+  'label' => 'LBL_CLG_PROGRAMS_CLG_DISCIPLINES_1_FROM_CLG_DISCIPLINES_TITLE',
+  'context' => 
+  array (
+    'link' => 'clg_programs_clg_disciplines_1',
+  ),
+);
---
+++ application/Ext/TableDictionary/tabledictionary.ext.php
@@ -0,0 +1,11 @@
+<?php
+// WARNING: The contents of this file are auto-generated.
+?>
+<?php
+// Merged from custom/Extension/application/Ext/TableDictionary/clg_programs_clg_disciplines_1.php
+ 
+ //WARNING: The contents of this file are auto-generated
+include('custom/metadata/clg_programs_clg_disciplines_1MetaData.php');
+
+
+?>
